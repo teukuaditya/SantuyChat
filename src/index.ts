846f52d794e3e5996692caf748929c91749e1117
teukuaditya <aditya20oktober@gmail.com>
2026-01-16 17:01:12 +0700

    solving error at mailtrap.ts

--- src/index.ts
+++ src/index.ts
@@ -27,4 +27,3 @@ app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
 });
 
-export default app;
--- src/utils/mailtrap.ts
+++ src/utils/mailtrap.ts
@@ -1,11 +1,16 @@
+import "dotenv/config";
 import { MailtrapClient } from "mailtrap";
 
-const TOKEN = process.env.MAILTRAP_TOKEN ?? "";
-const INBOX_ID = process.env.MAILTRAP_INBOX_ID ?? "";
+const TOKEN = process.env.MAILTRAP_TOKEN;
+const INBOX_ID = process.env.MAILTRAP_INBOX_ID;
+
+if (!TOKEN) throw new Error("MAILTRAP_TOKEN missing");
+if (!INBOX_ID) throw new Error("MAILTRAP_INBOX_ID missing");
 
 const mailtrap = new MailtrapClient({
   token: TOKEN,
-  testInboxId: Number.parseInt(INBOX_ID),
+  testInboxId: Number(INBOX_ID),
+  sandbox: true,
 });
 
 export default mailtrap;
